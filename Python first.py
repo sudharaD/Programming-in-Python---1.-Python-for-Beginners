a4f12e3be954fc6799ec5eefe3b1a8d64494fc93
sudharaD <sudharad7@gmail.com>
2022-10-09 22:46:15 +0530

    first 2 lecs

---
+++ Python first.py
@@ -0,0 +1,31 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, World!")
+Hello, World!
+>>> print(2**2)
+4
+>>> print(4**4)
+256
+>>> print(12%5)
+2
+>>> print(1+2**3/4*5)
+11.0
+>>> print(x = 1+2**3/4*5)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print(x = 1+2**3/4*5)
+TypeError: 'x' is an invalid keyword argument for print()
+>>> clear
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+>>> clean
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    clean
+NameError: name 'clean' is not defined
+>>> print(8-3*2)
+2
+>>> print(4-3*2)
+-2
