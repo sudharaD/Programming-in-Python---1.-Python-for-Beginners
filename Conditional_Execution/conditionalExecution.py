dda0a269c3dafafbcfe4846387bcf39360d56e10
sudharaD <sudharad7@gmail.com>
2022-10-13 23:05:24 +0530

    Conditional Execution

---
+++ Conditional_Execution/conditionalExecution.py
@@ -0,0 +1,70 @@
+time = 14
+
+if time > 12:
+    print("Good Afternoon")
+if time < 12:
+    print("Good Morning")
+print("Have a great day")
+
+age = 25
+
+if age > 18:
+    print("You can vote")
+else:
+    print("You can't vote")
+print("Thank ypu!")
+
+Mark = 50
+
+if Mark >= 75:
+    print("A")
+elif Mark >= 50:
+    print("C")
+else:
+    print("Repeat")
+print("Grade")
+
+m = int(100)
+p = 2000 / m
+print(p)
+
+mark = 55
+if mark <= 39: grade = 'D'
+elif mark < 55 : grade = 'C'
+elif mark < 70 : grade = 'B'
+else: grade = 'A'
+print (grade)
+
+m = int(input('Enter an integer: '))
+
+if (m < 1000):
+    p = 2000 / m
+else:
+    p = m/1000
+print(p)
+
+x = int(input('Enter x: '))
+if (x > 100):
+       k = x * 2
+       if (x >= 200):
+              k = k * 3
+       elif (k < 300):
+              k = k * 4
+       else:
+              k = k * 5
+else:
+       k = x * 3
+print(k)
+
+x = int(input('Enter x: '))
+if x > 100:
+    k = x * 2
+    if x >= 200:
+        k = k * 3
+    elif k < 300:
+        k = k * 4
+    else:
+        k = k * 5
+else:
+    k = x * 3
+print (k)
