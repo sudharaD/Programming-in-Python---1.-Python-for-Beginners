8bf16593468adfc82df9f7e30d9ce9fb614f78a3
sudharaD <sudharad7@gmail.com>
2022-10-18 23:51:57 +0530

    python functions

---
+++ Functions/functions.py
@@ -0,0 +1,20 @@
+print(min(5, 10))
+print(max(5, 10))
+
+# def functionname(parameters):
+#     "function_docstring"
+#     function_suit
+#     return[expression]
+
+def printme(str):
+    "Put a text message to the screen"
+    print(str)
+    return
+
+# exercise 1
+def greet(name):
+    "Greet a person with name"
+    print("Hello!", name)
+    return
+
+greet(input("What is your name? : "))
