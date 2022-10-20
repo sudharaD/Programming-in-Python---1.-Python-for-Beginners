31a47bce8eba846c6ac3c1f82fc97c1497d1199b
sudharaD <sudharad7@gmail.com>
2022-10-21 00:49:54 +0530

    Libraries

---
+++ Python Libraries/libraries.py
@@ -0,0 +1,12 @@
+import math
+
+# selectively import math module only
+# from math import sqrt
+# print(sqrt(num1)) <------ if only import sqrt use this
+
+
+num1 = 10
+sqrtRoot = 10 ** (1/2)
+print(sqrtRoot)
+
+print(math.sqrt(num1))
