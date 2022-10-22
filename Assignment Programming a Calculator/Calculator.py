a454719c737b0193d025e53454f1eba51aba8e3b
sudharaD <sudharad7@gmail.com>
2022-10-23 00:01:34 +0530

    Calculator error fixing

--- Assignment Programming a Calculator/Calculator.py
+++ Assignment Programming a Calculator/Calculator.py
@@ -7,11 +7,19 @@ def calculation(choice, first_number, second_number):
     elif choice == "*":
         result = first_number * second_number
     elif choice == "/":
-        result = first_number / second_number
+        if second_number == 0:
+            print("float division by zero")
+            return "None"
+        else:
+            result = first_number / second_number
     elif choice == "^":
         result = first_number ^ second_number
     elif choice == "%":
-        result = first_number % second_number
+        if second_number == 0:
+            print("float division by zero")
+            return "None"
+        else:
+            result = first_number % second_number
     else:
         result = "Unrecognized operation"
 
@@ -21,20 +29,38 @@ def select_op(choice):
 
     display_result = -1
 
-    if choice == "#":
-        return display_result
-    elif choice == "$":
-        return 1
+    if choice not in ("+", "-", "*", "/", "^", "%", "#", "$"):
+        print("Unrecognized operation")
     else:
-        first_number = float(input("Enter first number: "))
-        second_number = float(input("Enter second number: "))
-
-        result = calculation(choice, first_number, second_number)
-
-        if result == "Unrecognized operation":
-            print("Unrecognized operation")
+        if choice == "#":
+            return display_result
+        elif choice == "$":
+            return 1
         else:
-            print(first_number, choice, second_number, "=", result)
+            
+            # validate input only numbers
+            def validate_only_intrger(number):
+                try:
+                    number = float(number)
+                except ValueError:
+                    print(number, "not an integer!")
+                    return 1
+            
+            first_number = float(input("Enter first number: "))
+            validate_only_intrger(first_number)
+            print(int(first_number))
+            second_number = float(input("Enter second number: "))
+            validate_only_intrger(second_number)
+            print(int(second_number))
+
+            result = calculation(choice, first_number, second_number)
+
+            if result == "Unrecognized operation":
+                print(result)
+            elif result == "Something Went Wrong":
+                print(result)
+            else:
+                print(first_number, choice, second_number, "=", result)
 
 while True:
     print("Select operation.")
